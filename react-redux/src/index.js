f3f507daa28aa929b925d50579741b26c64f3daf
hekb001 <1259974928@qq.com>
2020-09-01 10:10:50 +0800

    test

--- react-redux/src/index.js
+++ react-redux/src/index.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React from 'react'; 
 import ReactDOM from 'react-dom';
-import { browserHistory, Router } from 'react-router';
+import { browserHistory, Router} from 'react-router';
 import allRoutes from './routes';
 import { syncHistoryWithStore } from 'react-router-redux';
 
