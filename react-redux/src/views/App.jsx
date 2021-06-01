247d1b954a92569701c06f53803cc415741280af
kevin.he <kevin.he@que360.com>
2021-06-01 16:11:38 +0800

    添加renderpropæ共享组件状态

--- react-redux/src/views/App.jsx
+++ react-redux/src/views/App.jsx
@@ -5,6 +5,24 @@ import { FormattedMessage } from 'react-intl';
 import cookie from 'js-cookie';
 import '../assets/styles/index.less';
 const langType = cookie.get('langType');
+export function Cat(props) {
+  const { x, y } = props.mouse;
+  console.log('%c'+ x,'color:green')
+  console.log('%c'+ y,'color:green')
+  return (<div style={{ position: 'absolute', left: x, top: y }}>kevin</div>)
+}
+export function Mouse(props) {
+  const [clientInfo, setClientInfo] = useState({ x: 0, y: 0 })
+  const handleMouseMove = (event) => {
+    setClientInfo({
+      x: event.clientX,
+      y: event.clientY
+    })
+  }
+  return (<div style={{ height: '100vh' }} onMouseMove={(event)=>handleMouseMove(event)}>
+    {props.render(clientInfo)}
+  </div>)
+}
 export default function App() {
   const goHome = () => {
     browserHistory.push('/home')
@@ -15,7 +33,6 @@ export default function App() {
     location.reload();
   }
   useEffect(() => {
-
   });
   return (
     <div className="App">
@@ -30,6 +47,11 @@ export default function App() {
       <div onClick={goHome}>
         <FormattedMessage id='home.item2' />
       </div>
+      <Mouse render={(mouse)=>
+        <Cat mouse={mouse}/>
+      }>
+
+      </Mouse>
     </div>
   );
 }
