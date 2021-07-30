ab70388acb2eb38b3e3dde4f749744ea9f8a6ab3
kevin.he <kevin.he@que360.com>
2021-07-30 10:41:41 +0800

    添加注释

--- react-redux/src/components/AuthCert/authElement.js
+++ react-redux/src/components/AuthCert/authElement.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: kevin.he 
+ * @Date: 2021-07-30 10:40:28 
+ * @Last Modified by: kevin.he
+ * @Last Modified time: 2021-07-30 10:40:50
+ * 元素添加权限
+ */
+
 import React, { useEffect } from 'react';
 import { Button } from 'antd';
 import { splitObj } from 'utils';
--- react-redux/src/components/AuthCert/index.js
+++ react-redux/src/components/AuthCert/index.js
@@ -2,7 +2,8 @@
  * @Author: kevin.he 
  * @Date: 2021-07-29 19:33:11 
  * @Last Modified by: kevin.he
- * @Last Modified time: 2021-07-29 19:38:22
+ * @Last Modified time: 2021-07-30 10:40:07
+ * 路由添加权限
  */
 
 import React, { Component } from 'react';
